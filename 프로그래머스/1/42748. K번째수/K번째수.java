cecdd97890d12086eec4202016f84ea20f7e0415
Andy <97105216+happyAndy0412@users.noreply.github.com>
2026-03-08 21:37:27 +0900

    [level 1] Title: K번째수, Time: 0.46 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+ 
+        for(int i=0;i<answer.length;i++){
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+            //2,5 -> 1, 4
+            int[] tmp = Arrays.copyOfRange(array, start-1, end);
+            
+            Arrays.sort(tmp);
+            
+            answer[i] = tmp[k-1];
+        }
+        
+        return answer;
+    
+    }
+}

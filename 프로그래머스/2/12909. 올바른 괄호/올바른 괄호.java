739e95fdef7794872b54fd47e6a889a78ed9896d
Andy <97105216+happyAndy0412@users.noreply.github.com>
2026-03-22 22:44:29 +0900

    [level 2] Title: 올바른 괄호, Time: 4.90 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+class Solution {
+boolean solution(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else {
+  
+                if (count == 0) {
+                    return false;
+                }
+                count--;
+            }
+        }
+
+
+        return count == 0;
+    }
+}
